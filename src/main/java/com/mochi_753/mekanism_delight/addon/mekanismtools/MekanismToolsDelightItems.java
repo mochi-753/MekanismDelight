f776ceb78355a3c2e56919ac7306d78eabe77811
mochi-753 <naka.mochi11@gmail.com>
2025-12-21 20:50:57 +0900

    ナイフ修正

--- src/main/java/com/mochi_753/mekanism_delight/addon/mekanismtools/MekanismToolsDelightItems.java
+++ src/main/java/com/mochi_753/mekanism_delight/addon/mekanismtools/MekanismToolsDelightItems.java
@@ -3,8 +3,10 @@ package com.mochi_753.mekanism_delight.addon.mekanismtools;
 import com.mochi_753.mekanism_delight.MekanismDelight;
 import com.mochi_753.mekanism_delight.register.MekanismDelightCreativeTab;
 import mekanism.tools.common.config.MekanismToolsConfig;
+import mekanism.tools.common.material.MaterialCreator;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.Tiers;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -16,17 +18,21 @@ public class MekanismToolsDelightItems {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, MekanismDelight.MOD_ID);
 
     public static final RegistryObject<Item> BRONZE_KNIFE = ITEMS.register("bronze_knife",
-            () -> new KnifeItem(Tiers.IRON, 0.5F, -2.0F, new Item.Properties().durability(MekanismToolsConfig.tools.bronze.getUses())));
+            () -> knifeItem(Tiers.IRON, MekanismToolsConfig.tools.bronze));
     public static final RegistryObject<Item> LAPIS_LAZULI_KNIFE = ITEMS.register("lapis_lazuli_knife",
-            () -> new KnifeItem(Tiers.IRON, 0.5F, -2.0F, new Item.Properties().durability(MekanismToolsConfig.tools.lapisLazuli.getUses())));
+            () -> knifeItem(Tiers.STONE, MekanismToolsConfig.tools.lapisLazuli));
     public static final RegistryObject<Item> OSMIUM_KNIFE = ITEMS.register("osmium_knife",
-            () -> new KnifeItem(Tiers.IRON, 0.5F, -2.0F, new Item.Properties().durability(MekanismToolsConfig.tools.osmium.getUses())));
+            () -> knifeItem(Tiers.IRON, MekanismToolsConfig.tools.osmium));
     public static final RegistryObject<Item> REFINED_GLOWSTONE_KNIFE = ITEMS.register("refined_glowstone_knife",
-            () -> new KnifeItem(Tiers.IRON, 0.5F, -2.0F, new Item.Properties().durability(MekanismToolsConfig.tools.refinedGlowstone.getUses())));
+            () -> knifeItem(Tiers.DIAMOND, MekanismToolsConfig.tools.refinedGlowstone));
     public static final RegistryObject<Item> REFINED_OBSIDIAN_KNIFE = ITEMS.register("refined_obsidian_knife",
-            () -> new KnifeItem(Tiers.IRON, 0.5F, -2.0F, new Item.Properties().durability(MekanismToolsConfig.tools.refinedObsidian.getUses())));
+            () -> knifeItem(Tiers.NETHERITE, MekanismToolsConfig.tools.refinedObsidian));
     public static final RegistryObject<Item> STEEL_KNIFE = ITEMS.register("steel_knife",
-            () -> new KnifeItem(Tiers.IRON, 0.5F, -2.0F, new Item.Properties().durability(MekanismToolsConfig.tools.steel.getUses())));
+            () -> knifeItem(Tiers.DIAMOND, MekanismToolsConfig.tools.steel));
+
+    private static KnifeItem knifeItem(Tier tier, MaterialCreator material) {
+        return new KnifeItem(tier, material.getAttackDamageBonus() - 2.0F, -2.0F, new Item.Properties().durability(material.getUses()));
+    }
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
@@ -34,9 +40,7 @@ public class MekanismToolsDelightItems {
 
     public static void onBuildCreativeModeTabContents(BuildCreativeModeTabContentsEvent event) {
         if (event.getTab().equals(MekanismDelightCreativeTab.TAB_MEKANISM_DELIGHT.get())) {
-            ITEMS.getEntries().forEach(registryObject -> {
-                event.accept(registryObject.get());
-            });
+            ITEMS.getEntries().forEach(registryObject -> event.accept(registryObject.get()));
         }
     }
 }
