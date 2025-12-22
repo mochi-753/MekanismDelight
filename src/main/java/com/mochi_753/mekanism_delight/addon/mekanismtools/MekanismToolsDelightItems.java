debfdbde002bafa94fa1c25e098c9aee7b550c81
mochi-753 <naka.mochi11@gmail.com>
2025-12-22 14:07:29 +0900

    ナイフの攻撃力を修正

--- src/main/java/com/mochi_753/mekanism_delight/addon/mekanismtools/MekanismToolsDelightItems.java
+++ src/main/java/com/mochi_753/mekanism_delight/addon/mekanismtools/MekanismToolsDelightItems.java
@@ -18,20 +18,20 @@ public class MekanismToolsDelightItems {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, MekanismDelight.MOD_ID);
 
     public static final RegistryObject<Item> BRONZE_KNIFE = ITEMS.register("bronze_knife",
-            () -> knifeItem(Tiers.IRON, MekanismToolsConfig.tools.bronze));
+            () -> mekanismKnifeItem(Tiers.IRON, MekanismToolsConfig.tools.bronze));
     public static final RegistryObject<Item> LAPIS_LAZULI_KNIFE = ITEMS.register("lapis_lazuli_knife",
-            () -> knifeItem(Tiers.STONE, MekanismToolsConfig.tools.lapisLazuli));
+            () -> mekanismKnifeItem(Tiers.STONE, MekanismToolsConfig.tools.lapisLazuli));
     public static final RegistryObject<Item> OSMIUM_KNIFE = ITEMS.register("osmium_knife",
-            () -> knifeItem(Tiers.IRON, MekanismToolsConfig.tools.osmium));
+            () -> mekanismKnifeItem(Tiers.IRON, MekanismToolsConfig.tools.osmium));
     public static final RegistryObject<Item> REFINED_GLOWSTONE_KNIFE = ITEMS.register("refined_glowstone_knife",
-            () -> knifeItem(Tiers.DIAMOND, MekanismToolsConfig.tools.refinedGlowstone));
+            () -> mekanismKnifeItem(Tiers.DIAMOND, MekanismToolsConfig.tools.refinedGlowstone));
     public static final RegistryObject<Item> REFINED_OBSIDIAN_KNIFE = ITEMS.register("refined_obsidian_knife",
-            () -> knifeItem(Tiers.NETHERITE, MekanismToolsConfig.tools.refinedObsidian));
+            () -> mekanismKnifeItem(Tiers.NETHERITE, MekanismToolsConfig.tools.refinedObsidian));
     public static final RegistryObject<Item> STEEL_KNIFE = ITEMS.register("steel_knife",
-            () -> knifeItem(Tiers.DIAMOND, MekanismToolsConfig.tools.steel));
+            () -> mekanismKnifeItem(Tiers.DIAMOND, MekanismToolsConfig.tools.steel));
 
-    private static KnifeItem knifeItem(Tier tier, MaterialCreator material) {
-        return new KnifeItem(tier, material.getAttackDamageBonus() - 3.5F, -2.0F, new Item.Properties().durability(material.getUses()));
+    private static KnifeItem mekanismKnifeItem(Tier tier, MaterialCreator material) {
+        return new KnifeItem(tier, material.getAttackDamageBonus() - 1.5F, -2.0F, new Item.Properties().durability(material.getUses()));
     }
 
     public static void register(IEventBus eventBus) {
