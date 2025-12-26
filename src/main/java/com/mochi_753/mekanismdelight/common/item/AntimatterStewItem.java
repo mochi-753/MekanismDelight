2214937cd5078eafc9aa3991f7b51ad6280282ac
mochi-753 <naka.mochi11@gmail.com>
2025-12-26 20:17:46 +0900

    に じ い ろ ！

--- src/main/java/com/mochi_753/mekanismdelight/common/item/AntimatterStewItem.java
+++ src/main/java/com/mochi_753/mekanismdelight/common/item/AntimatterStewItem.java
@@ -1,11 +1,16 @@
 package com.mochi_753.mekanismdelight.common.item;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.TextColor;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 import vectorwing.farmersdelight.common.item.ConsumableItem;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.awt.*;
 
 @ParametersAreNonnullByDefault
 public class AntimatterStewItem extends ConsumableItem {
@@ -17,5 +22,20 @@ public class AntimatterStewItem extends ConsumableItem {
     public void affectConsumer(ItemStack stack, Level level, LivingEntity consumer) {
         level.explode(consumer, consumer.getX(), consumer.getY(), consumer.getZ(), level.getRandom().nextFloat() * 64.0F, Level.ExplosionInteraction.MOB);
         consumer.setHealth(0.0F);
+    }
+
+    @Override
+    public @NotNull Component getName(ItemStack pStack) {
+        Component component = super.getName(pStack);
+        MutableComponent result = Component.empty();
+
+        for (int i = 0; i < component.getString().length(); i++) {
+            float hue = (System.currentTimeMillis() % 3000L) / 3000.0F + i / 20.0F;
+            int color = Color.HSBtoRGB(hue, 1.0F, 1.0F);
+            result.append(Component.literal(String.valueOf(component.getString().charAt(i)))
+                    .withStyle(style -> style.withColor(TextColor.fromRgb(color))));
+        }
+
+        return result;
     }
 }
